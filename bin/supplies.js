cf862a60fbd112f79d23be7e9fe9ee3028872039
Stella <sajcic.stella@gmail.com>
2020-06-15 21:04:35 +0200

    feat: multi-blockchain support

--- bin/supplies.js
+++ bin/supplies.js
@@ -13,6 +13,7 @@ program
   .option('-r, --reference <type>', 'Reference is a unique id for a specific driver; for example a smart contract address')
   .option('-m, --modifiers [addresses]', 'Wallets addresses to subtract from the total supply; for example wallets hold by the foundation or burn addresses. Split the addresses with a comma.')
   .option('-R, --record', 'Record the requests and parameters, and save them as fixtures')
+  .option('-B, --blockchain <type>', 'Define blockchain if driver supports multi-blockchain')
   .option('-k, --key <type>', 'APIkey when required')
   .option('--nocache', 'Disable the cache')
   .action(async (driverName, options) => {
@@ -27,11 +28,13 @@ program
       reference: options.reference,
       decimals: options.decimals,
       modifiers,
+      blockchain: options.blockchain,
     });
 
     let nockDone;
     if (options.record) {
       let fixture = `${driverName}.json`;
+
       if (coin && typeof coin.reference !== 'undefined') {
         fixture = `${driverName}-${coin.reference}.json`;
       }
@@ -55,6 +58,11 @@ program
       driver.secret = options.key;
     }
 
+    if (driver.supports.blockchains.length > 0 && !driver.supports.blockchains.includes(options.blockchain)) {
+      console.log('Driver supports multi-blockchain, define at least one to proceed.');
+      process.exit(1);
+    }
+
     const supply = await driver.getSupply(coin);
 
     if (options.record) {
@@ -65,6 +73,7 @@ program
           coins = fs.readFileSync(`${coinsDir}/${driverName}.json`);
           coins = JSON.parse(coins);
         }
+
         coins.push(coin);
         fs.writeFileSync(`${coinsDir}/${driverName}.json`, JSON.stringify(coins, null, 2), { flag: 'w+' });
       }
--- tests/drivers.int.test.js
+++ tests/drivers.int.test.js
@@ -124,6 +124,7 @@ drivers.forEach((driverName) => {
       }
     }
 
+    // specific token/coin
     if (driver.supports.tokens) {
       const tokens = coins.filter((coin) => (typeof coin.reference !== 'undefined'));
 
@@ -224,6 +225,82 @@ drivers.forEach((driverName) => {
           }
         });
       });
+    }
+
+    // multiblockchain
+    if (driver.supports.blockchains && !driver.supports.native) {
+      const [multiBlock] = coins.filter((coin) => !!coin.blockchain);
+
+      test('getSupply happy path', async () => {
+        await nock.back(`${driverName}.json`);
+        const supply = await driver.getSupply(multiBlock);
+        if (driver.supports.total) {
+          expect(supply.total).toBeGreaterThan(0);
+        }
+        if (driver.supports.circulating) {
+          expect(supply.circulating).toBeGreaterThan(0);
+        }
+        if (driver.supports.max) {
+          expect(supply.max).toBeGreaterThan(0);
+        }
+      });
+
+      if (driver.supports.total) {
+        test('Total supply should be greater than zero', async () => {
+          await nock.back(`${driverName}.json`);
+          const totalSupply = await driver.fetchTotalSupply(multiBlock.blockchain);
+          expect(totalSupply).toBeGreaterThan(0);
+        });
+      }
+
+      if (driver.supports.circulating) {
+        test('Circulating supply should be greater than zero', async () => {
+          await nock.back(`${driverName}.json`);
+          const circulatingSupply = await driver.fetchCirculatingSupply(multiBlock.blockchain);
+          expect(circulatingSupply).toBeGreaterThan(0);
+        });
+
+        if (driver.supports.total) {
+          test('Circulating supply should be less than or equal to total supply', async () => {
+            await nock.back(`${driverName}.json`);
+            const totalSupply = await driver.fetchTotalSupply(multiBlock.blockchain);
+            const circulatingSupply = await driver.fetchCirculatingSupply(multiBlock.blockchain);
+            expect(circulatingSupply).toBeLessThanOrEqual(totalSupply);
+          });
+        }
+      }
+
+      if (driver.supports.max) {
+        test('Max supply should be greater than zero', async () => {
+          await nock.back(`${driverName}.json`);
+          const maxSupply = await driver.fetchMaxSupply(multiBlock.blockchain);
+          expect(maxSupply).toBeGreaterThan(0);
+        });
+
+        if (driver.supports.total) {
+          test('Max supply should be greater than or equal to total supply', async () => {
+            await nock.back(`${driverName}.json`);
+            const totalSupply = await driver.fetchTotalSupply(multiBlock.blockchain);
+            const maxSupply = await driver.fetchMaxSupply(multiBlock.blockchain);
+            expect(maxSupply).toBeGreaterThanOrEqual(totalSupply);
+          });
+        }
+      }
+
+
+      if (driver.supports.balances) {
+        if (!Array.isArray(multiBlock.modifiers) || multiBlock.modifiers.length === 0) {
+          throw new Error('Native coin should have modifiers');
+        }
+
+        multiBlock.modifiers.forEach((modifier) => {
+          test('Modifier balance should be greater than or equal to zero', async () => {
+            await nock.back(`${driverName}.json`);
+            const balances = await driver.fetchBalance(modifier, multiBlock.blockchain);
+            expect(balances).toBeGreaterThanOrEqual(0);
+          });
+        });
+      }
     }
   });
 });
