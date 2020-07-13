cd882f8640cc9a87b9e7d83004fd7736a5cc2bc2
Stella <sajcic.stella@gmail.com>
2020-07-13 14:30:20 +0200

    feat: support multiblockchain fixtures

--- bin/supplies.js
+++ bin/supplies.js
@@ -7,18 +7,9 @@ const supplies = require('../lib/supplies');
 
 const coinsDir = path.join(__dirname, '..', 'tests', 'coins');
 
-/**
- * @param value
- * @param previous
- */
-function collect(value, previous) {
-  return previous.concat([value]);
-}
-
 program
   .command('get <driverName>')
   .option('-d, --decimals <type>', 'Some drivers like Etherscan require decimals')
-  .option('-c, --coin <value>', 'input a coin', collect, [])
   .option('-r, --reference <type>', 'Reference is a unique id for a specific driver; for example a smart contract address')
   .option('-m, --modifiers [addresses]', 'Wallets addresses to subtract from the total supply; for example wallets hold by the foundation or burn addresses. Split the addresses with a comma.')
   .option('-R, --record', 'Record the requests and parameters, and save them as fixtures')
@@ -27,44 +18,22 @@ program
   .option('--nocache', 'Disable the cache')
   .action(async (driverName, options) => {
     let useCache = true;
-
     let modifiers = [];
+    let nockDone;
+
     if (options.modifiers) {
       modifiers = options.modifiers.split(',');
     }
 
-    const coins = [];
+    const coin = new supplies.Coin({
+      reference: options.reference,
+      decimals: options.decimals,
+      modifiers,
+      blockchain: options.blockchain,
+    });
 
-    if (options.coin.length > 0) {
-      // eslint-disable-next-line no-restricted-syntax
-      for (const c of options.coin) {
-        const coin = new supplies.Coin(JSON.parse(c));
-        coins.push(coin);
-      }
-    } else {
-      const coin = new supplies.Coin({
-        reference: options.reference,
-        decimals: options.decimals,
-        modifiers,
-        blockchain: options.blockchain,
-      });
-      coins.push(coin);
-    }
-
-    let nockDone;
-    if (options.record) {
-      const fixture = `${driverName}.json`;
-
-      ({ nockDone } = await nock.back(
-        fixture,
-        defaultOptions(options.key),
-      ));
-
+    if (options.record || options.nocache) {
       // Disable the cache when in record mode
-      useCache = false;
-    }
-
-    if (options.nocache) {
       useCache = false;
     }
 
@@ -76,30 +45,48 @@ program
       driver.secret = options.key;
     }
 
-    await coins.map(async (coin) => {
-      // eslint-disable-next-line max-len
-      if (driver.supports.blockchains.length > 1 && !driver.supports.blockchains.includes(coin.blockchain)) {
-        console.log('Driver supports multi-blockchain, define at least one to proceed.');
-        process.exit(1);
+    // eslint-disable-next-line max-len
+    if (driver.supports.blockchains.length > 1 && !driver.supports.blockchains.includes(options.blockchain)) {
+      console.log('Driver supports multi-blockchain, define at least one to proceed.');
+      process.exit(1);
+    }
+
+    // create fixture file
+    if (options.record) {
+      let fixture = `${driverName}.json`;
+
+      if (driver.supports.blockchains.length > 1 && !coin.reference) {
+        fixture = `${driverName}-${coin.blockchain}.json`;
+      } else if (driver.supports.blockchains.length > 1 && !!coin.reference) {
+        fixture = `${driverName}-${coin.blockchain}-${coin.reference}.json`;
+      } else if (coin.reference) {
+        fixture = `${driverName}-${coin.reference}.json`;
       }
 
-      const supply = await driver.getSupply(coin);
-      console.log(supply);
-    });
+      ({ nockDone } = await nock.back(
+        fixture,
+        defaultOptions(options.key),
+      ));
+    }
+
+    const supply = await driver.getSupply(coin);
 
     if (options.record) {
       nockDone();
-      if (coins) {
-        let fixtureCoins = [];
+      if (coin) {
+        let coins = [];
         if (fs.existsSync(`${coinsDir}/${driverName}.json`)) {
-          fixtureCoins = fs.readFileSync(`${coinsDir}/${driverName}.json`);
-          fixtureCoins = JSON.parse(fixtureCoins);
+          coins = fs.readFileSync(`${coinsDir}/${driverName}.json`);
+          coins = JSON.parse(coins);
         }
 
-        fixtureCoins.push(...coins);
-        fs.writeFileSync(`${coinsDir}/${driverName}.json`, JSON.stringify(fixtureCoins, null, 2), { flag: 'w+' });
+        coins.push(coin);
+        fs.writeFileSync(`${coinsDir}/${driverName}.json`, JSON.stringify(coins, null, 2), { flag: 'w+' });
       }
     }
+
+    console.log(supply);
   });
+
 
 program.parse(process.argv);
